1dddcd9e89274069953691061d6d597983acfc67
docxia <810216472@qq.com>
2021-11-07 07:51:58 +0800

    Update chang.py

--- chang/chang.py
+++ chang/chang.py
@@ -408,7 +408,7 @@ while tx<nrows:
         N1c.append(tx)
     elif (z not in N1b) and (z not in N1a):
         print(z)
-        if "癌结节" in col[15][tx]:
+        if ("癌结节" in col[15][tx]) and (z==0):
             N1c.append(tx)    
     tx+=1
 #N0
